210f8f7a430b8b61021cd4e772f1bcf2c662219d
timothy-kumah <timothy.kumah@amalitech.com>
2024-04-23 14:32:35 +0000

    Removed Comments

--- DynamicNeo4jObject/Controllers/NodeController.cs
+++ DynamicNeo4jObject/Controllers/NodeController.cs
@@ -23,50 +23,6 @@ namespace DynamicNeo4jObject.Controllers
         [HttpPost("CreateNode")]
         public async Task<IActionResult> CreateModel(Dictionary<string,string> dto)
         {
-            //var companyName = "SomeCompany";
-
-            //var node = new Node
-            //{
-            //    UniqueName = $"{companyName}_{dto.Name}",
-            //    DisplayName = dto.Name,
-            //    Properties = dto.Properties
-            //};
-
-            //await _client.Cypher
-            //.Merge($"(n:{node.UniqueName} {{Id: $id}})")
-            //.OnCreate()
-            //.Set("n = $nodeData")
-            //.WithParams(new
-            //{
-            //    id = node.Id,
-            //    nodeData = new
-            //    {
-            //    node.Id,
-            //        UniqueName = node.UniqueName,
-            //        DisplayName = node.DisplayName
-            //    }
-            //})
-            ////.ForEach("(prop in $properties | MERGE (n)-[:HAS_PROPERTY]->(p:Property {Name: prop.Name}) SET p.Value = prop.Value)")
-            //.ForEach("(prop in $properties | n.")
-            //.WithParam("properties", node.Properties)
-            //.ExecuteWithoutResultsAsync();
-            //var dynamicProperties = new Dictionary<string, object>
-            //{
-            //    { "name", "John" },
-            //    { "age", 30 },
-            //    { "city", "New York" }
-            //    // Add more properties dynamically if needed
-            //};
-
-            //var companyName = "SomeCompany";
-
-            //var node = new Node
-            //{
-            //    UniqueName = $"{companyName}_{dto.Name}",
-            //    DisplayName = dto.Name,               
-            //};
-
-            // Create a node with dynamic properties using a Cypher query
             var companyName = "SomeCompany";
             var unique_name = $"{companyName}_{dto["Name"]}";
             var guid = Guid.NewGuid();
@@ -107,6 +63,28 @@ namespace DynamicNeo4jObject.Controllers
             return null;
         }
 
+        [HttpGet("GetNodes/{NodeName}/{id}")]
+        public async Task<IActionResult> FetchNodes(string NodeName,string Id)
+        {
+            var result = await _client.Cypher
+            .Match($"(n:{NodeName})")
+             .Return(n => n.As<string>())
+             .ResultsAsync;
+
+            if (result.Any())
+            {
+                var items = result.Select(x => JObject.Parse(x)["data"]);
+
+                var serialized = JsonConvert.SerializeObject(items, new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                    ContractResolver = new LowercaseContractResolver()
+                });
+
+                return Ok(serialized);
+            }
+            return null;
+        }
         public class LowercaseContractResolver : DefaultContractResolver
         {
             protected override string ResolvePropertyName(string propertyName)
